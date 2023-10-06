e5a2256947d21b230a6e63993ad4aae24b180ad2
nscando <nicolas.scandizzo@gmail.com>
2023-10-05 21:45:09 -0300

    ArreglosRegistros Contables - Arreglos - Validaciones

---
+++ LP3-TP3/RegistrosContables.aspx.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LP3_TP3
+    {
+    public partial class RegistrosContables : System.Web.UI.Page
+        {
+        protected void Page_Load ( object sender, EventArgs e )
+            {
+            if ( !IsPostBack )
+                {
+                // Puedes realizar acciones al cargar la página por primera vez aquí
+                }
+            }
+
+        protected void ButtonAgregar_Click ( object sender, EventArgs e )
+            {
+            // Verifica que se haya ingresado un monto y que ambos DropDownList tengan opciones seleccionadas
+            if ( !string.IsNullOrEmpty(TextBoxMonto.Text) && DropDownCuenta.SelectedIndex >= 0 && DropDownTipo.SelectedIndex >= 0 )
+                {
+                // Obtiene el valor seleccionado del DropDownList de "tipo"
+                int tipo = Convert.ToInt32(DropDownTipo.SelectedValue);
+
+                // Verifica que el valor del tipo sea 0 o 1
+                if ( tipo == 0 || tipo == 1 )
+                    {
+                    // Realiza la inserción a través del SqlDataSource
+                    SqlDataSourceRegistros.Insert();
+                    GridViewRegistros.DataBind();
+                    ClearControls();
+                    LabelMensaje.Text = "Registro agregado correctamente.";
+                    }
+                else
+                    {
+                    LabelMensaje.Text = "El valor del tipo debe ser 0 o 1.";
+                    }
+                }
+            else
+                {
+                LabelMensaje.Text = "Por favor, completa todos los campos.";
+                }
+            }
+
+
+
+
+
+        private void ClearControls ()
+            {
+
+            DropDownCuenta.SelectedIndex = 0;
+            TextBoxMonto.Text = "";
+            DropDownTipo.SelectedIndex = 0;
+            }
+        }
+    }
+    
---
+++ LP3-TP3/RegistrosContables.aspx.designer.cs
@@ -0,0 +1,125 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace LP3_TP3
+    {
+
+
+    public partial class RegistrosContables
+        {
+
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// Control LabelCuenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelCuenta;
+
+        /// <summary>
+        /// Control DropDownCuenta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownCuenta;
+
+        /// <summary>
+        /// Control LabelMonto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelMonto;
+
+        /// <summary>
+        /// Control TextBoxMonto.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox TextBoxMonto;
+
+        /// <summary>
+        /// Control LabelTipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelTipo;
+
+        /// <summary>
+        /// Control DropDownTipo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList DropDownTipo;
+
+        /// <summary>
+        /// Control ButtonAgregar.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button ButtonAgregar;
+
+        /// <summary>
+        /// Control LabelMensaje.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label LabelMensaje;
+
+        /// <summary>
+        /// Control GridViewRegistros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.GridView GridViewRegistros;
+
+        /// <summary>
+        /// Control SqlDataSourceCuentas.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSourceCuentas;
+
+        /// <summary>
+        /// Control SqlDataSourceRegistros.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.SqlDataSource SqlDataSourceRegistros;
+        }
+    }
